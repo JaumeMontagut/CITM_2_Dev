16917f48cd2216b06471c2b82eb523ba55085489
JaumeMontagut <36292291+JaumeMontagut@users.noreply.github.com>
2018-10-02 20:35:52 +0200

    Trying to count size of the string manually instead of with strlen()

--- 2018_09_21 - sting class/String/string.h
+++ 2018_09_21 - sting class/String/string.h
@@ -6,10 +6,12 @@ typedef unsigned int uint;
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
 
 class string {
 private:
 	char* text;
+	uint length;
 
 public:
 	//CONSTRUCTORS--------------------
@@ -17,17 +19,28 @@ public:
 	string() {
 	}
 
-	string(const string &otherString) {
-		text = new char[strlen(otherString.text)];
-		for (int i = 0; i < strlen(otherString.text); ++i) {
-			text[i] = otherString.text[i];
+	string(const char* text) {
+		//1.Allocate memory
+		//  - Calculate the length of the passed string
+		length = 0;
+		while (text[length] != '/0') {
+			length++;
+		}
+		//  - Allocate memory
+		this->text = new char[length];
+		//2.Associate each character
+		for (int i = 0; i < length; ++i) {
+			this->text[i] = text[i];
 		}
 	}
 
-	string(const char* text) {
-		this->text = new char[strlen(text)];
-		for (int i = 0; i < strlen(text); ++i) {
-			this->text[i] = text[i];
+	string(const string &otherString) {
+		
+		//1.Allocate memory
+		text = new char[strlen(otherString.text)];
+		//2.Associate each character
+		for (int i = 0; i < strlen(otherString.text); ++i) {
+			text[i] = otherString.text[i];
 		}
 	}
 
@@ -42,7 +55,16 @@ public:
 	}
 
 	bool operator== (const string &otherString) {
-
+		//1- Check if the length of the strings is the same
+		if (strlen(text) != strlen(otherString.text)) {
+			return false;
+		}
+		for (int i = 0; i < strlen(text); ++i) {
+			if (text[i] != otherString.text[i]) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	bool operator== (const char* otherText) {
