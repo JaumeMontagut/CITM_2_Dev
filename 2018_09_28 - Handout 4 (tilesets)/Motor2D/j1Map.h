99b5ac8f782ec97ea0d78844d62f0455da2331e4
JaumeMontagut <36292291+JaumeMontagut@users.noreply.github.com>
2018-10-03 11:26:28 +0200

    Renamed variables in header

--- 2018_09_28 - Handout 4 (tilesets)/Motor2D/j1Map.h
+++ 2018_09_28 - Handout 4 (tilesets)/Motor2D/j1Map.h
@@ -81,9 +81,9 @@ public:
 	bool Load(const char* path);
 
 	// TODO 8: Create a method that translates x,y coordinates from map positions to world positions
-	iPoint MapToWorld(int x, int y) const;
+	iPoint MapToWorld(int column, int row) const;
 
-	inline uint GetArrayPos(int x, int y) const;
+	inline uint GetArrayPos(int column, int row) const;
 
 private:
 
